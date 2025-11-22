9ac3a1e5c380ab3c440a395e2c8f3426fd48775d
AxEbr0 <bfigueredogonzalez@gmail.com>
2025-11-22 17:38:38 -0500

    Metodos de mostrar pendientes y cambiar a realizadas

--- GestorTareas.py
+++ GestorTareas.py
@@ -7,6 +7,8 @@ class GestorTareas:
     def addTarea(self, nomb, est):
         if nomb == "":
             print("Ingrese un nombre")
+        elif not est == "Pendiente" or not est == "Realizada":
+            print("Ingrese un estado correcto")
         else:
             nuevaTarea = Tarea(nomb, est)
             self.listaTareas.append(nuevaTarea)
@@ -17,8 +19,39 @@ class GestorTareas:
         else:
             print("Tareas:")
             i = 0
-            a = 2
             for tarea in self.listaTareas:
                 print(i++ +": " + tarea.getNombre + ", Estado: " + tarea.getEstado)
+
+
+    def mostrarTareasPendientes(self):
+        if self.listaTareas == []:
+            print("Esta lista de tareas esta vacía")
+        else:
+            print("Tareas sin realizar:")
+            i = 0
+            for tarea in self.listaTareas:
+                if tarea.getEstado == "Pendiente":
+                    print(i++ +": " + tarea.getNombre)
+
+    def marcarRealizada(self, pos):
+        cont = 0
+        if pos>len(self.listaTareas):
+            print("Fuera de rango")
+        else:
+            for a in range(0,len(self.listaTareas), 1):
+                if self.listaTareas[a].getEstado == "Pendiente":
+                    cont+=1
+                if cont == pos:
+                    self.listaTareas[a].setEstado == "Realizada"
+                    print("Tarea realizada con exito")
+                    break
+            
+        
+
+
+
+
+
+
             
             
